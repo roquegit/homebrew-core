3004a4cfcc0a2b5492d015f3c35aca8540913674
roqueporchetto@gmail.com <roqueporchetto@gmail.com>
2018-09-06 15:04:03 +0200

    correct ebulk formula

--- Formula/ebulk.rb
+++ Formula/ebulk.rb
@@ -1,27 +1,15 @@
 class Ebulk < Formula
   desc "Ingestion/download of large dataset into Wendelin-IA-Telecom platform"
-  homepage "https://softinst104003.host.vifib.net/erp5/web_site_module/fif_data_runner/#/"
-  url "https://lab.nexedi.com/rporchetto/embulk-wendelin/raw/master/tool_releases/SOURCE/ebulk_v3.0.zip"
-  sha256 "90a4bd96233366dba68e133a0b5ad4649fb7de0cdd0f99357ccbf151d8ce0ad0"
+  homepage "https://lab.nexedi.com/rporchetto/ebulk"
+  url "https://lab.nexedi.com/rporchetto/ebulk/repository/archive.tar.gz?ref=v1.0.0"
+  sha256 "0f04ba5e18ac6008d4376c4a14ce319ed3926afa1b22cc641389dad2442f84bf"
 
   bottle :unneeded
 
-  depends_on :java => "1.8"
-
   def install
-    # Execute through /bin/bash to be compatible with OS X 10.9.
-    libexec.install "ebulk_v3.0.zip"
-    (bin/"roque-ebulk").write <<~EOS
-      #!/bin/bash
-      export JAVA_HOME=$(#{Language::Java.java_home_cmd("1.8")})
-      exec /bin/bash "#{libexec}/ebulk_v3.0.zip" "$@"
-    EOS
+	libexec.install "ebulk"
+	libexec.install "ebulk-data"
+	bin.write_exec_script (libexec/"ebulk")
   end
 
-#  test do
-#    system bin/"embulk", "example", "./try1"
-#    system bin/"embulk", "guess", "./try1/seed.yml", "-o", "config.yml"
-#    system bin/"embulk", "preview", "config.yml"
-#    system bin/"embulk", "run", "config.yml"
-#  end
 end
